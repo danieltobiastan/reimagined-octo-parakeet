8454187f0d2e0b404563f436bdd99c14f1555f86
sumit951012 <84025039+sumit951012@users.noreply.github.com>
2021-05-15 16:08:19 +0800

    score page html and css

--- app/models.py
+++ app/models.py
@@ -23,8 +23,9 @@ class User(UserMixin, db.Model):
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Float, index=True, nullable=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    username = db.Column(db.String(64))
+    score = db.Column(db.Float, index=True, nullable=True) #WPM 
+    accuracy = db.Column(db.Float, index=True, nullable=True) #accuracy
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
@@ -44,3 +45,7 @@ class Score(db.Model):
 #>>> db.session.commit()
 
 #select user_id, username, score from User,Score where User.id  == Score.user_id; (see all records)
+
+# need to get a json file of user_id, score, timestamp and accracy
+
+#select Score.username, Score.score, Score.accuracy from User, Score where User.id == Score.user_id;
--- run.py
+++ run.py
@@ -6,3 +6,4 @@ def make_shell_context():
     return {'db':db,'User':User, 'Score':Score}
 
 # export FLASK_APP=run.py
+# export FLASK_DEBUG=1
